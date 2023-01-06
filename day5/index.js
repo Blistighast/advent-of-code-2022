b9b6d158d1d8898c7ad794e784b249b6ed22bb39
Brian Guterl <brianguterl99@gmail.com>
2023-01-06 18:44:54 -0500

    completed day 5 part 1 and 2

--- day5/index.js
+++ day5/index.js
@@ -6,6 +6,7 @@ const example = readFileSync("example.txt", { encoding: "utf-8"})
 const input = readFileSync("input.txt", { encoding: "utf-8"})
 .replace(/\r/g, "") // remove all \r characters
 
+// format input to be readable for functions
 function parseInput(input) {
   // split stack info from moves and split each by line w/ map
   const [rawStacks, rawMoves] = input.split("\n\n").map(line => line.split("\n"))
@@ -53,23 +54,69 @@ function parseInput(input) {
 const parsedExample = parseInput(example)
 const parsedInput = parseInput(input)
 
-// go through all the moves to move the boxes
-function playMove([stacks, moves]) {
-  //make deep copy of stacks
-  const localStacks = JSON.parse(JSON.stringify(stacks))
-  for (const move of moves) {
-    
-  }
 
-}
-
-
-// give boxes at top of each stack
-function day5part1([stacks, moves]) {
+// part 1
+// give boxes at top of each stack after all moves have been done
+function day5part1([stacks, moves]) { 
   const topOfStack = []
 
+  //make deep copy of stacks so it doesnt accidentaly effect part 2
+  const localStacks = JSON.parse(JSON.stringify(stacks))
 
-  return topOfStack
+// go through all the moves to move the boxes to appropriate stack, one box at a time
+  for (const move of moves) {
+    for (let i = 0; i < move.amount; i++) {
+      //take box off top of given stack
+      const box = localStacks[move.from].pop()
+  
+      //move box to stack it needs to go to and place on top
+      localStacks[move.to].push(box)
+    }
+  }
+
+  // read each box at the top of each stack after all moves have been done
+  for (let stack in localStacks) {
+    const topBox = localStacks[stack].slice(-1)[0]
+    topOfStack.push(topBox)
+  }
+
+  // alternative way to read the top box on stacks
+  // const topOfStack = indexs.map(value => {
+  //   const stack = localStacks[value]
+  //   return stack[stack.length - 1]
+  // })
+
+  //join them for proper format
+  return topOfStack.join("")
 }
 
 console.log(day5part1(parsedExample))
+console.log(day5part1(parsedInput))
+
+
+// part 2
+function day5part2([stacks, moves]) {
+  const topOfStack = []
+
+  //make copy of stacks to not effect anything else
+  const localStacks = JSON.parse(JSON.stringify(stacks))
+
+  // go through moves able to move multiple boxes at a time
+  for (let move of moves) {
+    // const boxesMoved = localStacks[move.from].splice(localStacks[move.from].length - move.amount) same thing but longer
+    const boxesMoved = localStacks[move.from].splice(-move.amount)
+    localStacks[move.to].push(...boxesMoved)
+  }
+
+  //read off boxes on top of stacks
+  for (let stack in localStacks) {
+    const topBox = localStacks[stack].slice(-1)[0]
+    topOfStack.push(topBox)
+  }
+
+  //join them for proper format
+  return topOfStack.join("")
+}
+
+console.log(day5part2(parsedExample))
+console.log(day5part2(parsedInput))
